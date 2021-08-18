f072bc7c3edfa3086732e271ca05e8b08b46f520
AbdulHafeez AbdulRaheem <switchdevelop1@gmail.com>
2021-08-18 13:15:57 +0100

    feat: hooked remove file to controller

--- controllers/imageuploader.controller.js
+++ controllers/imageuploader.controller.js
@@ -2,15 +2,19 @@ const catchAsync = require("../utils/catchAsync");
 const logger = require("../utils/logger");
 const path = require('path');
 const { cloudinaryHelper } = require('../helpers');
+const removeFile = require('../utils/removeFile');
 const uploadImage = catchAsync(async (req, res) => {
-  const imageUploadResponse  = await cloudinaryHelper.uploadImageTocloudinary(path.join(__dirname, `/../uploads/${req.file.filename}`));
+  const filePath = path.join(__dirname, `/../uploads/${req.file.filename}`);
+  const imageUploadResponse  = await cloudinaryHelper.uploadImageTocloudinary(filePath);
 
   const urlArray = imageUploadResponse.secure_url.split('upload');
   urlArray[0] = urlArray[0] + `upload/c_thumb,g_face,h_${process.env.HEIGHT},w_${process.env.WIDTH}`
   let processedUrl = urlArray[0] + urlArray[1];
 
-  console.log(imageUploadResponse);
   res.send(processedUrl);
+
+  // Remove file after upload
+  removeFile(filePath);
 });
 
 module.exports = {
